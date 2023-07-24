9f3fb3d1e00792d59b1d56c3a34e1c6389fa3489
Ryan Batchelor <ryanjbatch@gmail.com>
2023-07-23 20:03:19 -0500

    login

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,9 +1,18 @@
 import axios from 'axios'
 
-const API_URL = '/api/users'
+const API_URL = '/api/users/'
 
 const register = async (userData) => {
     const response = await axios.post(API_URL, userData)
+
+    if(response.data){
+        localStorage.setItem('user', JSON.stringify(response.data))
+    }
+    return response.data
+}
+
+const login = async (userData) => {
+    const response = await axios.post(API_URL + 'login', userData)
 
     if(response.data){
         localStorage.setItem('user', JSON.stringify(response.data))
@@ -18,7 +27,8 @@ const logout = () => {
 
 const authService = {
     register,
-    logout
+    logout,
+    login
 }
 
 export default authService
